ed7340eaa0bebd6aa0b05abb6db39c28e153504f
unknown <ankits.8080@gmail.com>
2023-01-27 14:59:45 +0530

    added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const cors=require("cors");
+require("dotenv").config();
 const app=express();
 app.use(express.json());
 app.use(cors());
@@ -15,6 +16,6 @@ app.get("/",(req,res)=>{
 app.use("/user",userRoute)
 
 app.listen("8080",async()=>{
-    await mongoose.connect("mongodb+srv://ankit:ankit@cluster0.beo4ggn.mongodb.net/mock14");
+    await mongoose.connect(process.env.DB_CONN);
     console.log("server started on port 8080")
 })
